4e6c1c033cd5812286e72d8d9bfbda0398d254b2
VeronicaCorMe <vcordobam@unal.edu.co>
2023-05-21 19:11:26 -0500

    pregunta3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,25 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+def split_lines(line):
+    return line.split(",")
+
+def select_column(line, x):
+    return line[x]
+
+def blancos(line):
+    linea= line.replace("\n", "")
+    linea= linea.replace("\r", "")
+    return linea
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line=blancos(line)
+        map_lines = split_lines(line)
+        col0= select_column(map_lines,1)
+        col1= select_column(map_lines,0)
+        sys.stdout.write("{},{}\n".format(col0,col1))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+def split_lines(line):
+    return line.split(",")
+
+def select_column(line, x):
+    return line[x]
+def blancos(line):
+    linea= line.replace("\n", "")
+    return linea
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line= blancos(line)
+        map_lines = split_lines(line)
+        col0= select_column(map_lines,1)
+        col1= select_column(map_lines,0)
+        sys.stdout.write("{},{}\n".format(col0,col1))
+
